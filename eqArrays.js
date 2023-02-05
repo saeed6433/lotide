27a286144ebbe21dfa6fd0b953298c65319bd25b
Saeed <saeed6433@gmail.com>
2023-02-05 17:46:15 -0500

    Equal Arrays

---
+++ eqArrays.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual , expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertation Passed: ${actual} === ${expected}`);
+  }
+  else {
+    console.log(`❌ Assertation Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1,arr2){
+  for (let i = 0; i< arr1.length; i++) {
+    //console.log(arr1[i], arr2[i])
+    if (arr1[i] !== arr2[i]){
+      return false;
+    } 
+  } return true;
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);// => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false

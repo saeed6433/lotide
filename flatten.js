cdf9c780c1d8b529a062f5a79781d3d4d46fbcf7
Saeed <saeed6433@gmail.com>
2023-02-06 00:15:21 -0500

    flatten

---
+++ flatten.js
@@ -0,0 +1,29 @@
+const eqArrays = function(arr1,arr2){
+  for (let i = 0; i< arr1.length; i++) {
+    //console.log(arr1[i], arr2[i])
+    if (arr1[i] !== arr2[i]){
+      return false;
+    } 
+  } return true;
+}
+
+const assertArraysEqual = function (ar1,ar2) {
+  if (eqArrays(ar1,ar2)){
+    console.log('The arrays are equal')
+  }
+  else console.log('The arrays are NOT equal');
+}
+
+const flatten = function (array) {
+  let flat = []
+  for (el of array){
+    if (Array.isArray(el)){
+      for(e of el) {flat.push(e)}
+    }
+    else flat.push(el)
+  }  
+  //console.log(flat);
+  return flat
+}
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6])

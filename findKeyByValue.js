ffb415bce3c3a082a18c5d9aea22477ed4871af3
Saeed <saeed6433@gmail.com>
2023-02-08 19:20:04 -0500

    find key by value

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual , expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertation Passed: ${actual} === ${expected}`);
+  }
+  else {
+    console.log(`❌ Assertation Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function (obj , val) {
+  for (let key in obj){
+    if (obj[key] === val){
+      return (key)
+    }
+  }
+}
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);

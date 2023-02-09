bb9956c40f76e1e5a7dc8dd0c21532925fc89260
Saeed <saeed6433@gmail.com>
2023-02-09 00:01:25 -0500

    update console log

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -18,7 +18,7 @@ const eqArrays = function(arr1,arr2){
 
 const assertArraysEqual = function (ar1,ar2) {
   if (eqArrays(ar1,ar2)){
-    console.log('✅ Assertation Passed: Two arrays are equal')
+    console.log(`✅ Assertation Passed: ${ar1} and ${ar2} are equal`)
   }
   else console.log('❌ Assertation Failed: Two arrays are NOT equal');
 }
